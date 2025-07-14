fb2f62f6ceb9a469c9e8d27f1a7714bde71305b9
Farhan Muzakki <116181890+farhanmuzakki213@users.noreply.github.com>
2025-07-14 21:07:44 +0700

    Update JurusanSeeder.php

--- database/seeders/JurusanSeeder.php
+++ database/seeders/JurusanSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Jurusan;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class JurusanSeeder extends Seeder
 {
@@ -13,40 +14,18 @@ class JurusanSeeder extends Seeder
      */
     public function run(): void
     {
-        Jurusan::factory()->create([
-            'id_jurusan' => '1',
-            'kode_jurusan' => 'AN',
-            'nama_jurusan' => 'Administrasi Niaga',
-        ]);
-        Jurusan::factory()->create([
-            'id_jurusan' => '2',
-            'kode_jurusan' => 'AK',
-            'nama_jurusan' => 'Akuntansi',
-        ]);
-        Jurusan::factory()->create([
-            'id_jurusan' => '3',
-            'kode_jurusan' => 'BI',
-            'nama_jurusan' => 'Bahasa Inggris',
-        ]);
-        Jurusan::factory()->create([
-            'id_jurusan' => '4',
-            'kode_jurusan' => 'EE',
-            'nama_jurusan' => 'Teknik Elektro',
-        ]);
-        Jurusan::factory()->create([
-            'id_jurusan' => '5',
-            'kode_jurusan' => 'ME',
-            'nama_jurusan' => 'Teknik Mesin',
-        ]);
-        Jurusan::factory()->create([
-            'id_jurusan' => '6',
-            'kode_jurusan' => 'SP',
-            'nama_jurusan' => 'Teknik Sipil',
-        ]);
-        Jurusan::factory()->create([
-            'id_jurusan' => '7',
-            'kode_jurusan' => 'TI',
-            'nama_jurusan' => 'Teknologi Informasi',
-        ]);
+        Jurusan::truncate();
+
+        // Siapkan data dalam bentuk array
+        $data = [
+            ['id_jurusan' => '1', 'kode_jurusan' => 'AN', 'nama_jurusan' => 'Administrasi Niaga'],
+            ['id_jurusan' => '2', 'kode_jurusan' => 'AK', 'nama_jurusan' => 'Akuntansi'],
+            ['id_jurusan' => '3', 'kode_jurusan' => 'BI', 'nama_jurusan' => 'Bahasa Inggris'],
+            ['id_jurusan' => '4', 'kode_jurusan' => 'EE', 'nama_jurusan' => 'Teknik Elektro'],
+            ['id_jurusan' => '5', 'kode_jurusan' => 'ME', 'nama_jurusan' => 'Teknik Mesin'],
+            ['id_jurusan' => '6', 'kode_jurusan' => 'SP', 'nama_jurusan' => 'Teknik Sipil'],
+            ['id_jurusan' => '7', 'kode_jurusan' => 'TI', 'nama_jurusan' => 'Teknologi Informasi'],
+        ];
+        Jurusan::insert($data);
     }
 }
